694b74a4c7cce820e9d4cb4cd238230d346190e2
Aditya dubey <adityaliberty33@gmail.com>
2024-02-12 22:42:20 +0530

    DB connection done

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,16 @@
 import express from "express";
-
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 const app = express();
-
+dotenv.config();
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => {
+    console.log("DB is connected!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 app.listen(3000, () => {
   console.log("server is running on port 3000!!");
 });
